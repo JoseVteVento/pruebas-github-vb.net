a4b5cfacb06fa3df7dbe9dbc524f34f455399f09
Jose Vte Vento <jvvento@gmail.com>
2015-06-03 16:51:45 +0200

    Primer commit de prueba para plataforma vb .net

---
+++ Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_mostrar_mensaje = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_mostrar_mensaje
+        '
+        Me.btn_mostrar_mensaje.Location = New System.Drawing.Point(102, 61)
+        Me.btn_mostrar_mensaje.Name = "btn_mostrar_mensaje"
+        Me.btn_mostrar_mensaje.Size = New System.Drawing.Size(75, 23)
+        Me.btn_mostrar_mensaje.TabIndex = 0
+        Me.btn_mostrar_mensaje.Text = "Button1"
+        Me.btn_mostrar_mensaje.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 145)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 167)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_mostrar_mensaje)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn_mostrar_mensaje As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private numeroMensaje As Integer = 0
+    Private mensaje1 As String = "Primer mensaje"
+    Private mensaje2 As String = "Segundo mensaje"
+    Private mensaje3 As String = "Tercer mensaje"
+    Private mensaje4 As String = "Ya no me quedan más mensajes. Tendremos que volver a empezar"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'colocar el texto de los botones y labels
+        btn_mostrar_mensaje.Text = "Mostrar mensaje"
+        Label1.Text = "Mensaje"
+
+        Label1.Visible = False
+        Label2.Visible = False
+
+
+    End Sub
+
+
+    Private Sub btn_mostrar_mensaje_Click(sender As Object, e As EventArgs) Handles btn_mostrar_mensaje.Click
+
+        'mostrar los elementos que mostrarán los mensajes
+        Label1.Visible = True
+        Label2.Visible = True
+
+        Select Case numeroMensaje
+            Case 0
+                Label2.Text = mensaje1
+                numeroMensaje = +1
+            Case 1
+                Label2.Text = mensaje2
+                numeroMensaje = +2
+            Case 2
+                Label2.Text = mensaje3
+                numeroMensaje = +3
+            Case 3
+                'en el ultimo, pone la variable en el estado inicial
+                Label2.Text = mensaje4
+                numeroMensaje = 0
+        End Select
+
+
+
+    End Sub
+End Class
